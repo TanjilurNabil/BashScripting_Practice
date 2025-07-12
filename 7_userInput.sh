ad2e35e7ea6d6bc27874b405629a8cb505bc4738
TanjilurNabil <86222610+TanjilurNabil@users.noreply.github.com>
2025-07-12 13:26:39 +0600

    User Interaction in shell

---
+++ 7_userInput.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Enter Your Skill"
+read SKILL
+
+echo "Your $SKILL is hign in demand"
+
+echo "Enter your credentials"
+read -p 'USERNAME: ' USERNAME
+read -sp 'Password: ' PASS
+
+echo
+
+echo "Login Successfull: Welcome USER $USERNAME"
